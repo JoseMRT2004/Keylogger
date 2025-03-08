c2050b5d95c54007a2f5ef5138eb9c5903e07b65
Jose M. <20241548@itla.edu.do>
2025-03-08 15:03:09 -0400

    Add: persistent module for host-guest connection management

---
+++ persistent.py
@@ -0,0 +1,5 @@
+"""
+This module is responsible for creating the code to generate a persistent system,
+establishing a connection between the Host and Guest, and saving the information
+on the host computer.
+"""
